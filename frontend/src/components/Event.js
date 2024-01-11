8fcb1c0f9a958e925cd78e352682770374dfdf72
SamyukthaGundrala <gsamyuktha86@gmail.com>
2024-01-10 23:03:52 -0800

    event.js changes

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -7,21 +7,24 @@ const Event = ({event}) => {
 
     <Card className ="my-5 p-3 rounded">
       <Link to = {`./event/${event._id}`}>
-          <Card.Img src ={event.image} variant ='top'/>
+          <Card.Img src ={event.image} variant ='top' className='rounded'/>
       </Link>
       <Card.Body>
-      <Link to = {`./event/${event._id}`}>
-        <Card.Title as ='div' style={{ height: '4rem' }}>
+      <Link to = {`./event/${event._id}`} style ={{textDecoration: 'none'}}>
+        <Card.Title as ='div' style={{ height: '4rem', fontWeight: 400}}>
           <strong>{event.name}</strong>
-        </Card.Title>
+        </Card.Title> 
       </Link>  
         <Card.Text as ='div'>
-         {event.date_time}
-        </Card.Text>
-        <Card.Text as ='div'style={{ height: '4rem' }}>
+         <b>{event.date_time}</b>
+        </Card.Text><br/>
+        <Card.Text as ='div'style={{ height: '4rem', fontSize: '14px'}}>
           {event.location.venue}<br/>
           <i class="fa-solid fa-location-dot"></i> {event.location.address}
-        </Card.Text>
+        </Card.Text><br/>
+        <Card.Text as ='div'>Ticket Price: 
+         $ <b>{event.price}</b>
+        </Card.Text><br/>
       </Card.Body>
     </Card>
    
