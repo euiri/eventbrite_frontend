19505c15583cc2183008540b37138d328952fae6
euiri <57123626+euiri@users.noreply.github.com>
2024-01-12 00:15:50 -0800

    including header, footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-      <div>App</div>
+    <>
+      <Header/> 
+      <main>
+        <Container>
+          Welcome
+        </Container>
+      
+      </main>
+      <Footer/>
+    </>
   )
 }
 
