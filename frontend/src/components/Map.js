3a0cc24225b31e8e461541db44616fb0c30e0001
PoojithaAlam <poojithaalam@gmail.com>
2024-01-17 01:06:55 -0800

    EventScreen Styling fix

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Map = ({ address }) => {
   const formattedAddress = address.replace(/ /g, '+');
-  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_MAP_API_KEY}&q=${formattedAddress}`;
+  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBlCxpBxsL62WJ69Xpx8bY3TA_8fODvlYY&q=${formattedAddress}`;
   
 
   return (
     <iframe
       title="Google Maps"
-      width="200"
+      width="100"
       height="150"
       style={{ border: 0 }}
       loading="lazy"
